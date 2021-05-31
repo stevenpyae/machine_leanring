abcf928fde094bfffaffba9f68e5ebce6243427b
stevenpyae <84059575+stevenpyae@users.noreply.github.com>
2021-05-31 17:27:28 +0800

    Add files via upload

---
+++ 3. Classification/6. Decision Tree Classification/decision_tree.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 28 22:17:09 2021
+
+@author: Corbi
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Import Dataset
+dataset = pd.read_csv("Social_Network_Ads.csv")
+# retrieve information from the Indexes
+# Features in the First Columns, Last Column is the Dependant Variable
+X = dataset.iloc[:, :-1].values      #iloc stands for locate indexes [all rows, columns without last column]
+y = dataset.iloc[:, -1].values       # collect the indexes of the last column  
+
+# Splitting the Dataset into Training and Test Sets
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0) # Fixing the Seed here
+
+
+# Feature Scaling Not required To IMPROVE THE TRAINING PERFORMANCE (-3, +3)
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train) 
+X_test = sc.transform(X_test)
+
+
+# Training the Logistic Regression Model on the Training Set
+from sklearn.tree import DecisionTreeClassifier 
+classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0) #Criterion = 'Entropy'
+# Lots of parameters, Tuning later on, Need to read. 
+#Part 10, Tune your Model
+classifier.fit(X_train, y_train)
+
+# Predicting a new result
+print(classifier.predict(sc.transform([[30,87000]]))) 
+# Predict method needs 2d Array 
+# need to feature-scale the wanted predicted result
+
+# Predicting the Test Set Result
+y_pred = classifier.predict(X_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+# 62 correct of class 0, 6 incorrect of class 1 (bought but predicted not buy)
+# 29 correct of class 1, 3 incorrect of class 0 (didn't buy but predicted buy)
+print(accuracy_score(y_test, y_pred))
+# Accuracy of 0.91
+
+from matplotlib.colors import ListedColormap
+X_set, y_set = sc.inverse_transform(X_train), y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+                     np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Decision Tree Classification (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+from matplotlib.colors import ListedColormap
+X_set, y_set = sc.inverse_transform(X_test), y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+                     np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Decision Tree Classification (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
